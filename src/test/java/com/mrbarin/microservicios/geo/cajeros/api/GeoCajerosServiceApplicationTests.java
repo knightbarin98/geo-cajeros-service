6b80633c0e1d10e0a9a91396e97bacc617e693dd
ivansoto <lopezsotoivan01@gmail.com>
2021-07-26 22:54:48 -0500

    first commit

---
+++ src/test/java/com/mrbarin/microservicios/geo/cajeros/api/GeoCajerosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbarin.microservicios.geo.cajeros.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoCajerosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
